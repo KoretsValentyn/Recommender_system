99acdfebfbb93229fad999abe289ea7e34f9b154
koreyo <KoretsValentyn@gmail.com>
2020-01-25 21:58:47 +0200

    change project structure

--- recommender_engine/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RecommenderEngineConfig(AppConfig):
-    name = 'recommender_engine'
--- recommender_system/settings.py
+++ recommender_system/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recommender_engine.apps.RecommenderEngineConfig',
 ]
 
 MIDDLEWARE = [
